4614267ae4ea5370e6c0e7a3c3390ac021aae644
hey-virender <bikuchauhan786@gmail.com>
2026-08-26 13:36:35 +0530

    move onClick from icon to parent button

--- frontend/src/components/TaskComponent.tsx
+++ frontend/src/components/TaskComponent.tsx
@@ -5,7 +5,7 @@ const TaskComponent = ({task, onDelete}:{task:Task,onDelete:({task_id}:{task_id:
 
   return (
     <div  id={task.id.toString()} className={`h-32 w-full p-3 relative border-1 rounded-lg ${task.status == "todo" && "border-blue-500"} ${task.status == "in_progress" && "border-yellow-500"} ${task.status == "done" && "border-green-500"}`}>
-      <button className="absolute right-4 top-4 bg-red-500 text-white rounded-md"><MdDelete className="size-8" onClick={()=>onDelete({task_id:task.id})} /></button>
+      <button className="absolute right-4 top-4 bg-red-500 text-white rounded-md" onClick={()=>onDelete({task_id:task.id})}><MdDelete className="size-8"  /></button>
       <div>
         <p className='capitalize'>{task.title}</p>
       </div>
